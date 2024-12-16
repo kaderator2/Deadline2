28d78720732290cfb5f19c421fa27d0650a773b0
Kade Dean <77413927+kaderator2@users.noreply.github.com>
2024-12-16 12:52:44 -0700

    Fix KeyError when accessing deck configuration in Anki 24+
    Fixed a bug where the add-on would crash with a KeyError: 'conf' when trying to create new deadlines. This occurred because recent versions of Anki (24+) changed how deck configurations are stored and accessed.
    Changes made:
    1. Updated deck configuration access from by_name(deck)['conf'] to use the new API
    2. Now uses deck_obj.get("conf_id", 1) instead of accessing 'conf' directly
    3. Properly retrieves and creates deck configurations using current Anki methods:
    get_config() to access current configuration
    add_config_returning_id() to create new configurations
    Saves deck changes using proper deck object updates
    
    
    
    This fix maintains all existing functionality while making it compatible with current Anki versions. Tested working on Anki 24.11.

--- config.py
+++ config.py
@@ -6,47 +6,53 @@
 #              are seen by deadline.
 # License: GNU GPL v3 <www.gnu.org/licenses/gpl.html>
 
-import datetime, time, math
-from anki.hooks import wrap, addHook
-from aqt import *
-from aqt.qt import *
-from . import ConfigForm, CalForm
-from aqt.main import AnkiQt
+import datetime
+import math
+import time
+
+from anki.hooks import addHook, wrap
 from anki.utils import intTime
-from aqt.utils import showWarning, openHelp, getOnlyText, askUser, showInfo, openLink
+from aqt import *
+from aqt.main import AnkiQt
+from aqt.qt import *
+from aqt.utils import (askUser, getOnlyText, openHelp, openLink, showInfo,
+                       showWarning)
+
+from . import CalForm, ConfigForm
 
 
 class DeadlineDialog(QDialog):
     def __init__(self):
-        QDialog.__init__(self, parent=mw) #, Qt.Window)
+        QDialog.__init__(self, parent=mw)  # , Qt.Window)
 
         self.mw = aqt.mw
         self.deadlines = mw.addonManager.getConfig(__name__)
         self.deadlines.pop("test")
         self.form = ConfigForm.Ui_Dialog()
         self.form.setupUi(self)
-        self.setWindowTitle(_("Deadline") )
+        self.setWindowTitle(_("Deadline"))
         self.form.ProcessDeadlineBox.clicked.connect(self.callDeadlines)
         self.fillFields()
         self.setupSignals()
-        if(self.deadlines.get("oneOrMany","")=="Many"):
+        if self.deadlines.get("oneOrMany", "") == "Many":
             self.form.OneOrManyBox.setCurrentIndex(1)
         else:
             self.form.OneOrManyBox.setCurrentIndex(0)
         self.resize(500, 500)
-        self.Calwindow=QDialog(self)
-        self.LayoutForCal=CalForm.Ui_Dialog()
+        self.Calwindow = QDialog(self)
+        self.LayoutForCal = CalForm.Ui_Dialog()
         self.LayoutForCal.setupUi(self.Calwindow)
         self.LayoutForCal.pushButton.clicked.connect(self.readValues)
         self.exec()
 
     def callDeadlines(self):
         from . import manualDeadlines
-        tempString=str(self.form.OneOrManyBox.currentText())
-        if(tempString.find("Single")!=-1):
-            self.deadlines["oneOrMany"]="One"
+
+        tempString = str(self.form.OneOrManyBox.currentText())
+        if tempString.find("Single") != -1:
+            self.deadlines["oneOrMany"] = "One"
         else:
-            self.deadlines["oneOrMany"]="Many"
+            self.deadlines["oneOrMany"] = "Many"
         mw.addonManager.writeConfig(__name__, self.deadlines)
         manualDeadlines()
 
@@ -56,9 +62,11 @@ class DeadlineDialog(QDialog):
             if user != str(aqt.mw.pm.name):
                 continue
             for deck, deadline in self.deadlines["deadlines"].get(user).items():
-                if(deadline==""):
+                if deadline == "":
                     continue
-                self.form.fieldList.addItem("user:{{{}}} deck:{{{}}} date:{{{}}}".format(user, deck,deadline))
+                self.form.fieldList.addItem(
+                    "user:{{{}}} deck:{{{}}} date:{{{}}}".format(user, deck, deadline)
+                )
 
     def setupSignals(self):
         f = self.form
@@ -67,48 +75,68 @@ class DeadlineDialog(QDialog):
         f.buttonBox.helpRequested.connect(self.onHelp)
 
     def readValues(self):
-        if(self.LayoutForCal.checkBox_2.isChecked()):
-            if(not utils.askUser("Are you sure you want to continue? The Apply to all Sub-Decks Box is checked")):
+        if self.LayoutForCal.checkBox_2.isChecked():
+            if not utils.askUser(
+                "Are you sure you want to continue? The Apply to all Sub-Decks Box is checked"
+            ):
                 return
-        user=str(aqt.mw.pm.name)
-        year=self.LayoutForCal.calendarWidget.selectedDate().year()
-        month=self.LayoutForCal.calendarWidget.selectedDate().month()
-        day=self.LayoutForCal.calendarWidget.selectedDate().day()
-        date="{}-{}-{}".format(year,str(month).zfill(2),str(day).zfill(2))
+        user = str(aqt.mw.pm.name)
+        year = self.LayoutForCal.calendarWidget.selectedDate().year()
+        month = self.LayoutForCal.calendarWidget.selectedDate().month()
+        day = self.LayoutForCal.calendarWidget.selectedDate().day()
+        date = "{}-{}-{}".format(year, str(month).zfill(2), str(day).zfill(2))
         self.Calwindow.close()
-        if(not user in self.deadlines["deadlines"]):
-            self.deadlines["deadlines"][user]={}
-        tempString=str(self.form.OneOrManyBox.currentText())
-        if(tempString.find("Single")!=-1):
-            self.deadlines["oneOrMany"]="One"
+        if not user in self.deadlines["deadlines"]:
+            self.deadlines["deadlines"][user] = {}
+        tempString = str(self.form.OneOrManyBox.currentText())
+        if tempString.find("Single") != -1:
+            self.deadlines["oneOrMany"] = "One"
         else:
-            self.deadlines["oneOrMany"]="Many"
+            self.deadlines["oneOrMany"] = "Many"
         while self.LayoutForCal.listWidget.selectedIndexes():
-            deck = self.LayoutForCal.listWidget.item(self.LayoutForCal.listWidget.selectedIndexes()[0].row()).text()
-            self.LayoutForCal.listWidget.takeItem(self.LayoutForCal.listWidget.selectedIndexes()[0].row())
-            self.applyDeadlineForDeck(deck,date)
+            deck = self.LayoutForCal.listWidget.item(
+                self.LayoutForCal.listWidget.selectedIndexes()[0].row()
+            ).text()
+            self.LayoutForCal.listWidget.takeItem(
+                self.LayoutForCal.listWidget.selectedIndexes()[0].row()
+            )
+            self.applyDeadlineForDeck(deck, date)
         self.fillFields()
 
-    def applyDeadlineForDeck(self,deck,date):
+    def applyDeadlineForDeck(self, deck, date):
         user = str(aqt.mw.pm.name)
-        childIds = list(mw.col.decks.child_ids(deck))
-        if(childIds and not self.LayoutForCal.checkBox_2.isChecked()):
-            return
-        elif(childIds and self.LayoutForCal.checkBox_2.isChecked()):
-            for child in childIds:
-                childName=mw.col.decks.name(child)
-                self.applyDeadlineForDeck(childName,date)
-            return
-        # Only create a new config if there wasn't already an existing custom one
-        DeckIDToUpdate = mw.col.decks.id_for_name(deck)
-        self.deadlines["deadlines"][user][deck] = date
-        if (mw.col.decks.by_name(deck)['conf'] == 1):
-            tempID = mw.col.decks.add_config_returning_id(deck, mw.col.decks.config_dict_for_deck_id(DeckIDToUpdate))
-            deckToUpdate = mw.col.decks.by_name(deck)
-            deckToUpdate['conf'] = tempID
-            mw.col.decks.save(deckToUpdate)
-        mw.addonManager.writeConfig(__name__, self.deadlines)
 
+        childIds = list(mw.col.decks.child_ids(deck))
+        if childIds and not self.LayoutForCal.checkBox_2.isChecked():
+            return
+        elif childIds and self.LayoutForCal.checkBox_2.isChecked():
+            for child in childIds:
+                childName = mw.col.decks.name(child)
+                self.applyDeadlineForDeck(childName, date)
+            return
+
+        # Get the deck ID
+        DeckIDToUpdate = mw.col.decks.id_for_name(deck)
+
+        # Add the deadline to the config
+        self.deadlines["deadlines"][user][deck] = date
+
+        # Get the current deck config
+        deck_obj = mw.col.decks.get(DeckIDToUpdate)
+        current_config_id = deck_obj.get("conf_id", 1)  # default to 1 if not found
+
+        # Only create a new config if using the default
+        if current_config_id == 1:
+            # Get the current config dict
+            current_config = mw.col.decks.get_config(current_config_id)
+            # Create new config based on current settings
+            new_config_id = mw.col.decks.add_config_returning_id(deck, current_config)
+            # Update the deck to use the new config
+            deck_obj["conf_id"] = new_config_id
+            mw.col.decks.save(deck_obj)
+
+        # Save the deadlines config
+        mw.addonManager.writeConfig(__name__, self.deadlines)
 
     def onAdd(self):
         self.Calwindow.show()
@@ -116,22 +144,23 @@ class DeadlineDialog(QDialog):
         for deck in sorted(aqt.mw.col.decks.all_names()):
             self.LayoutForCal.listWidget.addItem(deck)
 
-
     def onDelete(self):
         while self.form.fieldList.selectedIndexes():
-            temp=self.form.fieldList.item(self.form.fieldList.selectedIndexes()[0].row()).text()
+            temp = self.form.fieldList.item(
+                self.form.fieldList.selectedIndexes()[0].row()
+            ).text()
             self.form.fieldList.takeItem(self.form.fieldList.selectedIndexes()[0].row())
-            fields=temp.split("}")
-            user=fields[0].split("{")[1]
-            deck=fields[1].split("{")[1]
-            date=fields[2].split("{")[1]
+            fields = temp.split("}")
+            user = fields[0].split("{")[1]
+            deck = fields[1].split("{")[1]
+            date = fields[2].split("{")[1]
             self.deadlines["deadlines"].get(user).pop(deck)
             mw.addonManager.writeConfig(__name__, self.deadlines)
-            delConfId=mw.col.decks.by_name(deck)['conf']
+            delConfId = mw.col.decks.by_name(deck)["conf"]
             # Don't even attempt to delete the default conf; otherwise we would get an error pop up
-            if(delConfId!=1):
+            if delConfId != 1:
                 mw.col.decks.remove_config(delConfId)
         # self.fillFields()
 
     def onHelp(self):
-        openLink('https://github.com/BSCrumpton/Deadline2')
+        openLink("https://github.com/BSCrumpton/Deadline2")
